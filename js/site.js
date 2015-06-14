386403ea3421a06ba8b0ba827346a16d02a81d49
Roger Steve Ruiz <hi@rog.gr>
2015-06-14 00:07:31 -0400

    [s] Bring in initial styles and prepare for JS

---
+++ js/site.js
@@ -0,0 +1,19 @@
+/*
+ * Social
+ * @see _includes/social-markup.html
+ * @selector .js-social
+ */
+;( function( win, doc ) {
+
+  $q = doc.querySelectorAll;
+  getId = doc.getElementById;
+
+  if ( 'function' === typeof $q ) {
+
+	// do you, bae
+
+  } else {
+	console.log( 'TODO' );
+  }
+
+} )( this, this.document );
